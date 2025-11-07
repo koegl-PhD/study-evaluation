80b2fdd04f7233d324b0555491f547e9bdedb3cc
koegl <koeglfryderyk@gmail.com>
2025-11-07 10:16:30 +0100

    change to boxplot (without the scatter plt)

--- performance_across_bins_plots.py
+++ performance_across_bins_plots.py
@@ -9,7 +9,7 @@ from scipy.stats import mannwhitneyu
 from statsmodels.stats.multitest import multipletests
 import matplotlib.patches as mpatches
 import matplotlib as mpl
-
+from scipy.stats import kruskal
 import utils
 
 
@@ -39,6 +39,20 @@ def pairwise_mwu_holm(y: pd.Series, groups: pd.Series) -> List[Tuple[str, str, f
     return [(a, b, float(p)) for (a, b), p in zip(pairs, p_adj)]
 
 
+def remove_y_ticks_smaller_than(g: sns.FacetGrid, threshold: float) -> None:
+    for ax in g.axes.flat:
+        y_min, y_max = ax.get_ylim()
+        ax.set_ylim(bottom=y_min, top=71)
+        ax.set_yticks([t for t in ax.get_yticks() if t >= 0 and t < 71])
+
+
+def remove_y_ticks_bigger_than(g: sns.FacetGrid, threshold: float) -> None:
+    for ax in g.axes.flat:
+        # get current yticks and keep only those < threshold
+        yticks = [t for t in ax.get_yticks() if t < threshold]
+        ax.set_yticks(yticks)
+
+
 def annotate_pairs(ax: plt.Axes, x_positions: Dict[str, float], y_top: float, pairs: List[Tuple[str, str, float]], experience: bool) -> None:
     """Draw brackets and significance stars for given pairs."""
     h = (y_top * 0.04) if y_top > 0 else 0.2
@@ -47,8 +61,9 @@ def annotate_pairs(ax: plt.Axes, x_positions: Dict[str, float], y_top: float, pa
     if experience:
         m, n, o = pairs
         pairs = [o, n, m]
-        # h += 10
+        cur += 9.6
     else:
+        cur -= 20
         pairs.reverse()
 
     for a, b, p in pairs:
@@ -69,8 +84,11 @@ def annotate_pairs(ax: plt.Axes, x_positions: Dict[str, float], y_top: float, pa
             mant = mant.rstrip("0").rstrip(".")
             exp = int(exp)
             label = f"$p={mant}\\times10^{{{exp}}}$"
+        else:
+            # now the lael is th exact value without scientific notation
+            label = f"$p={p:.3f}$"
 
-        ax.text((x1 + x2) / 2, cur + h * 1.0, label, ha="center", va="bottom")
+        ax.text((x1 + x2) / 2, cur + h * 1.2, label, ha="center", va="bottom")
         cur += h * 1.7
 
 
@@ -90,16 +108,16 @@ def plot_tre_violins(df: pd.DataFrame, out_path: str) -> None:
     )
 
     g = sns.catplot(
-        kind="violin",
+        kind="box",
         data=work,
         x="TRE_bin",
         y="bifurcation_error",
         col="experience",
-        inner=None,
+        # inner=None,
         # cut=0,
-        scale="width",
+        # scale="width",
         sharey=True,
-        linewidth=0.5,
+        linewidth=1.0,
     )
 
     for ax, (exp, df_exp) in zip(g.axes.flat, work.groupby("experience", sort=False)):
@@ -109,26 +127,36 @@ def plot_tre_violins(df: pd.DataFrame, out_path: str) -> None:
         # ax.scatter([x_pos[str(k)] for k in med.index.astype(str)],
         #            med.values, marker="x", s=50, zorder=3)
 
+        groups_error = [*[df_exp.loc[df_exp["TRE_bin"] == b, "bifurcation_error"]
+                      for b in df_exp["TRE_bin"].unique()]]
+        kw_stat, kw_p = kruskal(
+            *[df_exp.loc[df_exp["TRE_bin"] == b, "bifurcation_error"] for b in df_exp["TRE_bin"].unique()]
+        )
+        N_err, k_err = sum(len(g) for g in groups_error), len(groups_error)
+        eps2_err = utils.epsilon_squared_kw(
+            float(kw_stat), N_err, k_err)
+        print(f"Kruskal–Wallis H={kw_stat:.2f}, p={kw_p:.4f}, ε²={eps2_err:.4f} for bifurcation_error across TRE bins")
+
         p_table = pairwise_mwu_holm(
             df_exp["bifurcation_error"], df_exp["TRE_bin"].astype(str))
         y_top = df_exp["bifurcation_error"].max()
         annotate_pairs(ax, x_pos, float(y_top), p_table,
                        experience=exp == "Experienced")
-        ax.set_xlabel("TRE bin")
+        ax.set_xlabel("TRE bin", labelpad=15)
         ax.set_ylabel("Matching error ($mm$)")
 
         color = "#8f4926" if exp == "Inexperienced" else "#1d4e6d"
-        sns.stripplot(
-            data=df_exp,
-            x="TRE_bin",
-            y="bifurcation_error",
-            ax=ax,
-            color=mpl.colors.to_rgba(color, 1.0),        # black dots
-            size=4,           # point size
-            jitter=True,      # random horizontal offset
-            alpha=0.5,        # transparency
-            zorder=2,         # draw above violins
-        )
+        # sns.stripplot(
+        #     data=df_exp,
+        #     x="TRE_bin",
+        #     y="bifurcation_error",
+        #     ax=ax,
+        #     color=mpl.colors.to_rgba(color, 1.0),        # black dots
+        #     size=4,           # point size
+        #     jitter=True,      # random horizontal offset
+        #     alpha=0.5,        # transparency
+        #     zorder=2,         # draw above violins
+        # )
 
     titles = {
         "Experienced": r"Experienced",
@@ -139,32 +167,61 @@ def plot_tre_violins(df: pd.DataFrame, out_path: str) -> None:
         # find which experience this axis corresponds to
         exp = ax.get_title().split(" = ")[-1]
         if exp in titles:
-            ax.set_title(titles[exp], fontsize=13)
+            ax.set_title(titles[exp], fontsize=14)
 
     exp_colors = {
         "Experienced": "#86b2cb",
         "Inexperienced": "#f1cbb8"
     }
+    # for violin
+    """
     for ax in g.axes.flat:
         exp = ax.get_title().split(" = ")[-1]
         for pc in [c for c in ax.collections if isinstance(c, mpl.collections.PolyCollection)]:
             pc.set_facecolor(mpl.colors.to_rgba(exp_colors[exp], 1.0))
             pc.set_edgecolor("black")
             # pc.set_linewidth(0.5)
+    """
+    # for boxplot
+    for ax in g.axes.flat:
+        exp = ax.get_title().split(" = ")[-1]
+        for patch in [c for c in ax.patches if isinstance(c, mpl.patches.PathPatch)]:
+            patch.set_facecolor(exp_colors[exp])
+            patch.set_edgecolor("black")
+            patch.set_alpha(1.0)
 
     for ax, (exp, df_exp) in zip(g.axes.flat, work.groupby("experience", sort=False)):
         grouped = df_exp.groupby("TRE_bin")["bifurcation_error"]
         stats = grouped.agg(['count', 'mean', 'std'])
+
+        if exp == "Experienced":
+            offset_adjustment = 2.41
+        else:
+            offset_adjustment = 7.0
+
+        down_room = 0.06
+
+        y_min, y_max = ax.get_ylim()
+        y_offset = y_min + (y_max - y_min) * 0.00 - offset_adjustment
+
         for i, (label, row) in enumerate(stats.iterrows()):
+            txt = (
+                f"count$={int(row['count'])}$\n"
+                f"${row['mean']:.2f}\\pm{row['std']:.2f}\\,\\mathrm{{mm}}$"
+            )
             ax.text(
-                i, ax.get_ylim()[0] - (ax.get_ylim()[1] *
-                                       0.085),  # slightly below axis
-                f"count=${int(row['count'])}$\n${row['mean']:.2f}\\pm{row['std']:.2f}mm$",
-                ha="center", va="top", fontsize=12
+                i-0.37,
+                y_offset,
+                txt,
+                ha="left", va="bottom", fontsize=12,
+                bbox=dict(facecolor="white", edgecolor="gray",
+                          boxstyle="round,pad=0.3")
             )
 
-    for ax in g.axes.flat:
-        ax.set_xlabel("TRE bin", labelpad=35)
+        # extend y-axis slightly downward to make room
+        ax.set_ylim(y_min - (y_max - y_min) * down_room, y_max)
+
+    remove_y_ticks_smaller_than(g, 0.0)
 
     g.fig.tight_layout()
     g.fig.savefig(out_path, dpi=300)
